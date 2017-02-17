b749b346f2f627489f61afd97c5c9c6efceae13c
Derpanieux <kevint2001@gmail.com>
2017-02-17 18:40:51 -0500

    Cleans up things

--- desktop/src/com/amitycs/sandc/desktop/DesktopLauncher.java
+++ desktop/src/com/amitycs/sandc/desktop/DesktopLauncher.java
@@ -7,6 +7,8 @@ import com.amitycs.sandc.SupplyAndConquer;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.height = 750;
+		config.width = 1500;
 		new LwjglApplication(new SupplyAndConquer(), config);
 	}
 }
