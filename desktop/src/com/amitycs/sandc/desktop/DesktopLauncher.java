94611a3678a75ef92d54e738c83975f22540eb25
Derpanieux <kevint2001@gmail.com>
2017-02-17 17:52:32 -0500

    Sets up libGDX things

---
+++ desktop/src/com/amitycs/sandc/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.amitycs.sandc.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.amitycs.sandc.SupplyAndConquer;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SupplyAndConquer(), config);
+	}
+}
