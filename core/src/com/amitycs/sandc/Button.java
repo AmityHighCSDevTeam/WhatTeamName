0120d2ecc83771d6a157c54aaf6cf50ae934d378
Derpanieux <kevint2001@gmail.com>
2017-02-17 19:25:01 -0500

    Begins work on Menu and Buttons.

---
+++ core/src/com/amitycs/sandc/Button.java
@@ -0,0 +1,41 @@
+package com.amitycs.sandc;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+
+public class Button {
+	public Texture texture;
+	public int width;
+	public int height;
+	public int xPosition;
+	public int yPosition;
+	
+	public Button() {
+		setFields(0, 0, 0, 0, null);
+	}
+	
+	public Button(int xPos, int yPos, int width, int height, Texture t) {
+		setFields(xPos, yPos, width, height, null);
+	}
+	
+	public Button(Texture t) {
+		setFields(0, 0, 0, 0, t);
+	}
+	
+	private void setFields(int xPos, int yPos, int width, int height, Texture t){
+		this.xPosition = xPos;
+		this.yPosition = yPos;
+		this.width = width;
+		this.height = height;
+	}
+	
+	public boolean mousedOver() {
+		int x = Gdx.input.getX();
+		if (xPosition < x && x < xPosition + width){
+			int y =Gdx.input.getY();
+			if (yPosition < y && y < yPosition + height)
+				return true;
+		}
+		return false;
+	}
+}
--- core/src/com/amitycs/sandc/SupplyAndConquer.java
+++ core/src/com/amitycs/sandc/SupplyAndConquer.java
@@ -3,21 +3,17 @@ package com.amitycs.sandc;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class SupplyAndConquer extends Game {
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		super.render();
 	}
 
 	@Override
 	public void create() {
-		// TODO Auto-generated method stub
-		
+		setScreen(new MainMenuScreen());
 	}
 	
 }
